1a2569b5b1ca18ae260acdf7f3b83ad8243f9f06
HaroshiyGrigoriy <guruwolf8@yandex.ru>
2025-12-03 03:00:11 +0300

    Начал верстку главной страницы саныча

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,20 @@ export default function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
 
-      {/* Главная страница Саныча */}
-      <Route path="/sanych" element={<SanychHome />} />
-
-      {/* Пока нет других страниц — просто добавим заглушки */}
-      <Route path="/sanych/modules" element={<div>Карта модулей</div>} />
-      <Route path="/sanych/start" element={<div>Старт кофейни</div>} />
-      <Route path="/sanych/checklists" element={<div>Чек-листы</div>} />
-      <Route path="/sanych/documents" element={<div>Документы</div>} />
-      <Route path="/sanych/calculator" element={<div>Калькуляторы</div>} />
-      <Route path="/sanych/staff" element={<div>Персонал</div>} />
-
-      {/* Пример глубины */}
-      <Route path="/sanych/start/premises" element={<div>Premises</div>} />
-      <Route path="/sanych/start/power" element={<div>Power</div>} />
-      <Route path="/sanych/start/layout" element={<div>Layout</div>} />
-
-      {/* И так далее — постепенно заменишь на настоящие компоненты */}
+      {/* <Route path="/sanych" element = {<SanychLayout />}>
+        <Route index element={<SanychHome/>} />
+        
+         {/* <Route path="/sanych/modules" element={<div>Карта модулей</div>} />
+         <Route path="/sanych/start" element={<div>Старт кофейни</div>} />
+         <Route path="/sanych/checklists" element={<div>Чек-листы</div>} />
+         <Route path="/sanych/documents" element={<div>Документы</div>} />
+         <Route path="/sanych/calculator" element={<div>Калькуляторы</div>} />
+         <Route path="/sanych/staff" element={<div>Персонал</div>} />
+         <Route path="/sanych/start/premises" element={<div>Premises</div>} />
+         <Route path="/sanych/start/power" element={<div>Power</div>} />
+         <Route path="/sanych/start/layout" element={<div>Layout</div>} />
+       */}
+      {/* </Route> */} */}
     </Routes>
   );
 }
